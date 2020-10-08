0a7bafd09c99799ca483c4ea9b4e7cb3ae063b96
Angry Monster <67206311+RKSouth@users.noreply.github.com>
2020-10-07 19:15:44 -0700

    added psuedo/starter code

---
+++ Develop/index.js
@@ -0,0 +1,35 @@
+const inquirer = require("inquirer");
+const fs = require('fs');
+const axios = require("axios");
+
+//don't forget npm init -y
+//npm instal axios --save
+
+// * At least one badge
+// * Project title
+// * Description
+// * Table of Contents
+// * Installation
+// * Usage
+// * License
+// * Contributing
+// * Tests
+// * Questions
+// * User GitHub profile picture
+// * User GitHub email
+const questions = [
+type: ``,
+message: "",
+name: '',
+default: '',
+validate: 
+];
+
+function writeToFile(fileName, data) {
+}
+
+function init() {
+
+}
+
+init();
