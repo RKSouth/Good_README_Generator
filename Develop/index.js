42abb4f4f90e70f67607fc75a624d8629c816109
Angry Monster <67206311+RKSouth@users.noreply.github.com>
2020-10-08 15:48:43 -0700

    copied files from today's activities to start converting

--- Develop/index.js
+++ Develop/index.js
@@ -1,35 +1,82 @@
 const inquirer = require("inquirer");
-const fs = require('fs');
-const axios = require("axios");
+const fs = require("fs");
+const util = require("util");
 
-//don't forget npm init -y
-//npm instal axios --save
+const writeFileAsync = util.promisify(fs.writeFile);
 
-// * At least one badge
-// * Project title
-// * Description
-// * Table of Contents
-// * Installation
-// * Usage
-// * License
-// * Contributing
-// * Tests
-// * Questions
-// * User GitHub profile picture
-// * User GitHub email
-const questions = [
-type: ``,
-message: "",
-name: '',
-default: '',
-validate: 
-];
-
-function writeToFile(fileName, data) {
+function promptUser() {
+  return inquirer.prompt([
+    {
+      type: "input",
+      name: "name",
+      message: "What is your name?"
+    },
+    {
+      type: "input",
+      name: "location",
+      message: "Where are you from?"
+    },
+    {
+      type: "input",
+      name: "hobby",
+      message: "What is your favorite hobby?"
+    },
+    {
+      type: "input",
+      name: "food",
+      message: "What is your favorite food?"
+    },
+    {
+      type: "input",
+      name: "github",
+      message: "Enter your GitHub Username"
+    },
+    {
+      type: "input",
+      name: "linkedin",
+      message: "Enter your LinkedIn URL."
+    }
+  ]);
 }
 
-function init() {
+function generateReadMe(answers) {
+  return `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+  <title>Document</title>
+</head>
+<body>
+  <div class="jumbotron jumbotron-fluid">
+  <div class="container">
+    <h1 class="display-4">Hi! My name is ${answers.name}</h1>
+    <p class="lead">I am from ${answers.location}.</p>
+    <h3>Example heading <span class="badge badge-secondary">Contact Me</span></h3>
+    <ul class="list-group">
+      <li class="list-group-item">My GitHub username is ${answers.github}</li>
+      <li class="list-group-item">LinkedIn: ${answers.linkedin}</li>
+    </ul>
+  </div>
+</div>
+</body>
+</html>`;
+}
 
+async function init() {
+  try {
+    const answers = await promptUser();
+
+    const html = generateReadMe(answers);
+
+    await writeFileAsync("readme.md", md);
+
+    console.log("Successfully wrote to read.me");
+  } catch(err) {
+    console.log(err);
+  }
 }
 
 init();
