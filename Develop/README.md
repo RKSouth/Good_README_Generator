b7c0e1e285adf0a2cb3c51654f5c7ae26d94ea50
Angry Monster <67206311+RKSouth@users.noreply.github.com>
2020-10-08 15:57:44 -0700

    got it to generate a readme

---
+++ Develop/README.md
@@ -0,0 +1,5 @@
+ rachael
+    rachael
+   
+    rachael
+       rachael
--- Develop/index.js
+++ Develop/index.js
@@ -40,29 +40,11 @@ function promptUser() {
 }
 
 function generateReadMe(answers) {
-  return `
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-  <title>Document</title>
-</head>
-<body>
-  <div class="jumbotron jumbotron-fluid">
-  <div class="container">
-    <h1 class="display-4">Hi! My name is ${answers.name}</h1>
-    <p class="lead">I am from ${answers.location}.</p>
-    <h3>Example heading <span class="badge badge-secondary">Contact Me</span></h3>
-    <ul class="list-group">
-      <li class="list-group-item">My GitHub username is ${answers.github}</li>
-      <li class="list-group-item">LinkedIn: ${answers.linkedin}</li>
-    </ul>
-  </div>
-</div>
-</body>
-</html>`;
+  return ` ${answers.name}
+    ${answers.location}
+   
+    ${answers.github}
+       ${answers.linkedin}`;
 }
 
 async function init() {
@@ -71,9 +53,9 @@ async function init() {
 
     const html = generateReadMe(answers);
 
-    await writeFileAsync("readme.md", md);
+    await writeFileAsync("README.md",html);
 
-    console.log("Successfully wrote to read.me");
+    console.log("Successfully wrote to README.md");
   } catch(err) {
     console.log(err);
   }
